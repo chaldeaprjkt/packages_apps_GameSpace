d5b20a865fa40fe1e149c7b4833ef297f0b8f909
Nauval Rizky <enuma.alrizky@gmail.com>
2021-12-06 04:26:59 +0700

    MenuSwitcher: simplify the view with compound drawable usage

--- app/src/main/java/io/chaldeaprjkt/gamespace/widget/MenuSwitcher.kt
+++ app/src/main/java/io/chaldeaprjkt/gamespace/widget/MenuSwitcher.kt
@@ -2,15 +2,9 @@ package io.chaldeaprjkt.gamespace.widget
 
 import android.content.Context
 import android.util.AttributeSet
-import android.util.Log
 import android.view.LayoutInflater
-import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.core.view.doOnLayout
-import androidx.core.view.doOnPreDraw
-import androidx.core.view.isVisible
 import io.chaldeaprjkt.gamespace.R
 import io.chaldeaprjkt.gamespace.data.AppSettings
 import io.chaldeaprjkt.gamespace.utils.dp
@@ -28,55 +22,42 @@ class MenuSwitcher : LinearLayout {
 
     private val appSettings by lazy { AppSettings(context) }
 
-    private val titleView: TextView?
-        get() = findViewById(R.id.text)
-
-    private val iconView: ImageView?
-        get() = findViewById(R.id.icon)
+    private val content: TextView?
+        get() = findViewById(R.id.menu_content)
 
     private var lastFpsValue = 0f
 
-    var icon = R.drawable.ic_arrow_right
-        set(value) {
-            iconView?.setImageResource(value)
-            field = value
-        }
-
     var showFps = false
         set(value) {
-            titleView?.isVisible = value
-            iconView?.isVisible = !value
-            if (!showFps) {
-                updateFpsValue(0f)
-            }
+            if (!value) updateFpsValue(0f)
             layoutParams.width = if (value) LayoutParams.WRAP_CONTENT else 36.dp
             field = value
         }
 
     var isDragged = false
         set(value) {
-            if (value)
-                icon = R.drawable.ic_drag
+            if (value && !showFps) setMenuIcon(R.drawable.ic_drag)
             field = value
         }
 
     fun updateFpsValue(newValue: Float) {
         if (lastFpsValue == newValue) return
-        if (newValue < 1) {
-            titleView?.text = ""
-        } else {
-            titleView?.text = newValue.roundToInt().toString()
-        }
+        content?.text = newValue.roundToInt().toString()
+        lastFpsValue = newValue
     }
 
     fun updateIconState(isExpanded: Boolean, location: Int) {
         showFps = if (isExpanded) false else appSettings.showFps
-        icon = when {
-            isExpanded -> {
-                R.drawable.ic_close
-            }
+        when {
+            isExpanded -> R.drawable.ic_close
             location > 0 -> R.drawable.ic_arrow_right
             else -> R.drawable.ic_arrow_left
-        }
+        }.let { setMenuIcon(it) }
+    }
+
+    private fun setMenuIcon(icon: Int = R.drawable.ic_close) {
+        val ic = if (showFps) null else resources.getDrawable(icon, context.theme)
+        content?.textScaleX = if (showFps) 1f else 0f
+        content?.setCompoundDrawablesRelativeWithIntrinsicBounds(null, ic, null, null)
     }
 }
